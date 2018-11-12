e0d048d818a50edb3b3de25c64a4bd935c3d8104
yaoling <245807889lois@gmail.com>
2018-11-12 13:35:21 +0000

    Q7 Reverse Integer

--- 7_ReverseInteger.java
+++ 7_ReverseInteger.java
@@ -1,23 +1,20 @@
-class Solution {
-    
-    public int a,b,c, result;
-    public int reverse(int x) {
-        
-        c = x % 10;
-        b = ((x - c) / 10) % 10;
-        a = (((x - c) / 10) - b) / 10;
-        
-        if (c != 0) {
-            result = c * 100 + b * 10 + a;
-        } else if (c == 0 && b != 0) {
-            result = b * 10 + a;
-        } else if (c == 0 && b == 0){
-            result = a;
-        }
-        if (x < 0) {
-            return -result;
-        }else {
-            return result;
+class Solution { 
+    public int reverse(int x) { 
+        int result = 0; 
+        String str = String.valueOf(x); 
+        for (int i = str.length()-1; i >= 0; i--) { 
+            if (i == 0 && str.charAt(i) == '-') { 
+                result *= -1; 
+            } else if (str.charAt(i) >= '0' && str.charAt(i) <= '9') {
+                result = result * 10; 
+                result = result + (int)str.charAt(i) - '0'; 
+            } else { 
+                return 0; 
+            } 
         } 
-    }
+
+        return result; 
+    } 
 }
+            
+
