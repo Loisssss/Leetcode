43a9d4bb519c8d381ed44ce38f001e7eca4da135
yaoling <245807889lois@gmail.com>
2018-11-18 12:36:36 +0000

    Hash Tables Ransom Note(HashMap)

--- HashTables_Ransom_Note.java
+++ HashTables_Ransom_Note.java
@@ -1,20 +1,27 @@
-	// Complete the checkMagazine function below.
-	static void checkMagazine(String[] magazine, String[] note) {
-		boolean flag = true;
-		List<String> list = new ArrayList<>();
-		for (int i = 0; i < magazine.length; i++) {
-			list.add(magazine[i]);
-		}
-		for (int i = 0; i < note.length; i++) {
-			if (!list.contains(note[i])) {
-				flag = false;
-			} else {
-				list.remove(note[i]);
-			}
-		}
-		if (list.size() >= 0 && flag == true) {
-			System.out.println("Yes");
-		} else {
-			System.out.println("No");
-		}
-	}
+    static void checkMagazine(String[] magazine, String[] note) {
+        String flag = "Yes";
+        HashMap<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < magazine.length; i++) {
+            if (!map.containsKey(magazine[i])) {
+                map.put(magazine[i], 1);
+            } else {
+                int value = map.get(magazine[i]);
+                value++;
+                map.put(magazine[i], value);
+            }
+        }
+        for (int i = 0; i < note.length; i++) {
+            if (!map.containsKey(note[i])) {
+                flag = "No";
+            } else {
+                int value = map.get(note[i]);
+                value--;
+                if (value < 0) {
+                    flag = "No";
+                } else {
+                    map.put(note[i], value);
+                }
+            }
+        }
+        System.out.println(flag);
+    }
